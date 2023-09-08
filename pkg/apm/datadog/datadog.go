5a1789f39f0b66bfe00f7ebe2aa24d62e23cbdc9
aryansharma9917 <sharmaaryan9837@gmail.com>
2023-09-08 18:21:02 +0530

    Added APM datadog test

--- pkg/apm/datadog/datadog.go
+++ pkg/apm/datadog/datadog.go
@@ -22,4 +22,38 @@ func  Start(addr string, serviceName string, debugMode bool) {
 }
 
 // Trace is a filter that traces incoming requests.
-func Trace
+func Trace(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
+	span, ctx := tracer.StartSpanFromContext(req.Request.Context(), req.Request.URL.Path,
+		tracer.SpanType(ext.SpanTypeWeb),
+		tracer.ResourceName(req.SelectedRoutePath()),
+		tracer.Tag(ext.HTTPMethod, req.Request.Method),
+		tracer.Tag(ext.HTTPURL, req.Request.URL.Path),
+	)
+	if spanctx, eww := tracer.Extract(tracer.HTTPHeadersCarrier(req.Request.Header)); eww == nil {
+		opts = append(opts, tracer.ChildOf(spanctx))
+	}
+
+	span, ctx := tracer.StartSpanFromContext(ctx, "http.request", opts...)
+	defer span.Finish()
+
+	//pass the span through the request context
+	req.Request = req.Request.WithContext(ctx)
+
+	chain.ProcessFilter(req, resp)
+
+	span.SetTag(ext.HTTPCode, strconv.Itoa(resp.StatusCode()))
+
+	if resp.Error() != nil {
+		span.SetTag(ext.Error, resp.Error())
+	}
+}
+
+// Inject adds tracer headed to a HTTP request
+func Inject(outRequest *http.Request, restfulRequest *restful.Request) error {
+	span, ok := tracer.SpanFromContext(restfulRequest.Request.Context())
+	if !ok {
+		return errors.New("no trace context in the request, request is not instrumented")
+	}
+
+	return tracer.Inject(span.Context(), tracer.HTTPHeadersCarrier(outRequest.Header))
+}
